b7d477fd055b338ef8a11b756dba8447fd2a1d16
IronJoo <joanaferro92@gmail.com>
2021-03-10 22:22:21 +0000

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace w3rFunction //{}
+{
+    class Exercise1
+    {
+        public static void Main(string[] args)
+        {
+            Welcome();
+
+            Console.ReadLine();
+        }
+
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome Friends!\nHave a nice day!");
+        }
+    }
+}
