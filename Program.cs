9b687e776f22044c3b9c61df7c96bd5dfc2452c0
IronJoo <joanaferro92@gmail.com>
2021-03-17 19:39:14 +0000

    Exercise 3

--- Program.cs
+++ Program.cs
@@ -2,20 +2,23 @@
 
 namespace w3rFunction //{}
 {
-    class Exercise2
+    class Exercise3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please input a name: ");
-            string name = Console.ReadLine();
-            Welcome(name);
+            Console.WriteLine("Function to calculate the sum of two numbers:\n--------");
+            Console.WriteLine("Please enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("The sum of {0} + {1} is {2}.", num1, num2, Sum(num1, num2));
 
             Console.ReadLine();
         }
 
-        public static void Welcome(string name)
+        public static int Sum(int num1, int num2)
         {
-            Console.WriteLine($"Welcome friend {name}!\nHave a nice day!");
+            return num1 + num2;
         }
     }
 }
